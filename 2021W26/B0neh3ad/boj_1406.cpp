ee39d78f4ddf4d3c9e04cc5cd02177269dc3dc3e
김진수 <js1044k@snu.ac.kr>
2021-07-04 00:43:56 +0900

    Update boj_1406.cpp

--- 2021W26/B0neh3ad/boj_1406.cpp
+++ 2021W26/B0neh3ad/boj_1406.cpp
@@ -1,6 +1,51 @@
-#include <stdio.h>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <string>
+using namespace std;
 
 int main(void){
-    
+    list<char> editor;
+    list<char>::iterator cursor;
+
+    cursor = editor.end();
+    string init;
+    cin >> init;
+    for(char& c : init){
+        editor.push_back(c);
+    }
+
+    int lines;
+    cin >> lines;
+    cin.ignore();
+
+    string cmd;
+    for(int i=0; i<lines; i++){
+        getline(cin, cmd);
+        switch(cmd[0]){
+        case 'L':
+            if(cursor != editor.begin()){
+                cursor = prev(cursor);
+            }
+            break;
+        case 'D':
+            if(cursor != editor.end()){
+                cursor = next(cursor);
+            }
+            break;
+        case 'B':
+            if(cursor != editor.begin()){
+                editor.erase(prev(cursor));
+            }
+            break;
+        case 'P':
+            editor.insert(cursor, cmd[2]);
+            break;
+        }
+    }
+
+    for(list<char>::iterator it = editor.begin(); it != editor.end(); it++){
+        cout << *it;
+    }
     return 0;
 }
