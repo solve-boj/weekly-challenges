e07e680e6b673596930f06205e78e45da63e9684
김진수 <js1044k@snu.ac.kr>
2021-07-09 02:28:06 +0900

    edit documentation of boj_17387.py

--- 2021W27/B0neh3ad/boj_17387.py
+++ 2021W27/B0neh3ad/boj_17387.py
@@ -5,6 +5,8 @@ def cross(x1, y1, x2, y2):
 def include_point(p1, p2, q):
     '''
     한 직선 위 세 점 p1, p2, q에 대하여 선분 p1p2가 q를 포함하는지 여부를 return
+    또는
+    직사각형의 대각선 꼭짓점 p1, p2와 평면 위 점 q에 대하여 직사각형이 q를 포함하는지 여부를 return
     '''
     return (q[0] - p1[0]) * (q[0] - p2[0]) <= 0 and (q[1] - p1[1]) * (q[1] - p2[1]) <= 0
 
@@ -24,6 +26,23 @@ def check_relative_position(std_line, cmp_line):
 
     return 1 if result < 0 else 0
 
+
+def is_crossed(line, shape):
+    '''
+    직선 line과 도형 shape 간 교점 존재 여부 return
+    '''
+    answer = 0
+    n = len(shape)
+    for i in range(n):
+        result = 0
+        result += check_relative_position(line, [shape[i], shape[(i+1)%n]])
+        result += check_relative_position([shape[i], shape[(i+1)%n]], line)
+
+        answer += int(result >= 2)
+        
+    return answer > 0
+    
+
 t = int(input())
 
 for _ in range(t):
@@ -31,10 +50,14 @@ for _ in range(t):
     line = [temp[0:2], temp[2:4]]
     square = [temp[4:6], temp[6:8]]
 
-    # 선분의 두 점이 직사각형 내에 있는 경우
+    answer = 'F'
+    # 선분의 두 점이 직사각형 내에 있는 경우 -> True
+    if include_point(square[0], square[1], line[0]) and include_point(square[0], square[1], line[1]):
+        answer = 'T'
+    # 아닌 경우 -> 검토
+    else:
+        if is_crossed(line, [square[0], [square[1][0], square[0][1]], square[1], [square[0][0], square[1][1]]]):
+            answer = 'T'
+    
+    print(answer)
 
-    result = 0
-    result += check_relative_position(p[:2], p[2:])
-    result += check_relative_position(p[2:], p[:2])
-
-    print(int(result >= 2))
