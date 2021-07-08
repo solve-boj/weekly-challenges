e2c50357e3faadc0bb058151fb0b072748dc9ca0
김진수 <js1044k@snu.ac.kr>
2021-07-09 00:00:04 +0900

    +1

---
+++ 2021W27/B0neh3ad/boj_2166.py
@@ -0,0 +1,15 @@
+def cross(x1, y1, x2, y2):
+    return x1*y2 - x2*y1
+
+
+n = int(input())
+p = []
+for i in range(n):
+    p.append(list(map(int, input().split())))
+
+area = 0
+for i in range(2, n):
+    area += cross(p[i-1][0] - p[0][0], p[i-1][1] - p[0][1], p[i][0] - p[0][0], p[i][1] - p[0][1])
+
+area = round(abs(area / 2), 1)
+print(area)
