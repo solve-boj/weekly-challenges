87dd025f8dbc117699e12edf420833a4f1659ffd
김진수 <js1044k@snu.ac.kr>
2021-07-08 22:25:56 +0900

    +1

---
+++ 2021W27/B0neh3ad/boj_11758.py
@@ -0,0 +1,9 @@
+def cross(x1, y1, x2, y2):
+    return x1*y2 - x2*y1
+
+p = []
+for i in range(3):
+    p.append(list(map(int, input().split())))
+
+det = cross(p[1][0] - p[0][0], p[1][1] - p[0][1], p[2][0] - p[0][0], p[2][1] - p[0][1])
+print(det if not det else det // abs(det))
