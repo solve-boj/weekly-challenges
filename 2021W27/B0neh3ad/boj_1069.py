6edc38efac48602491b308676600c85bd482d043
김진수 <js1044k@snu.ac.kr>
2021-07-09 12:05:17 +0900

    +1

---
+++ 2021W27/B0neh3ad/boj_1069.py
@@ -0,0 +1,16 @@
+x, y, d, t = map(int, input().split())
+dis = (x ** 2 + y ** 2) ** .5
+
+ans = 0
+k = 0
+
+if d > t:
+    if dis <= d:
+        ans = min(min(t + abs(dis - d), dis), 2*t)
+    else:
+        ans = (dis // d) * t
+        ans += min(t, abs(dis - (dis // d) * d))
+else:
+    ans = dis
+
+print(ans)
