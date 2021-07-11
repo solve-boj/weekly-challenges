b013bbad919d6b2691637c5936824e83bffd8f59
김진수 <js1044k@snu.ac.kr>
2021-07-12 00:05:47 +0900

    +1

--- 2021W27/B0neh3ad/pgr_42860.py
+++ 2021W27/B0neh3ad/pgr_42860.py
@@ -1,3 +1,19 @@
 def solution(name):
+    cp = []
+    updown = 0
+
+    for i in range(len(name)):
+        if name[i] != 'A':
+            cp.append(i)
+            updown += min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
+
     answer = 0
+    if len(cp) > 1:
+        answer = 2*len(name)
+        for i in range(1, len(cp)):
+            answer = min(answer, 2*cp[i-1] + len(name) - cp[i], cp[i-1] + 2*(len(name) - cp[i]))
+        answer += updown
+    else:
+        answer = (cp[0] if len(cp) else 0) + updown
+    
     return answer
