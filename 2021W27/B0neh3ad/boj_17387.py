4f71b231560c74e210113d9861084b65672af692
김진수 <js1044k@snu.ac.kr>
2021-07-26 22:41:00 +0900

    +0(재채점되고 실패 뜬 거 수정)

--- 2021W27/B0neh3ad/boj_17387.py
+++ 2021W27/B0neh3ad/boj_17387.py
@@ -1,67 +1,31 @@
-'''
-    선분 교차 2
-    https://www.acmicpc.net/problem/17387
-'''
 def cross(x1, y1, x2, y2):
     return x1*y2 - x2*y1
 
 
 def include_point(p1, p2, q):
-    '''
-    한 직선 위 세 점 p1, p2, q에 대하여 선분 p1p2가 q를 포함하는지 여부를 return
-    또는
-    직사각형의 대각선 꼭짓점 p1, p2와 평면 위 점 q에 대하여 직사각형이 q를 포함하는지 여부를 return
-    '''
     return (q[0] - p1[0]) * (q[0] - p2[0]) <= 0 and (q[1] - p1[1]) * (q[1] - p2[1]) <= 0
 
 
-def check_relative_position(std_line, cmp_line):
+def check_relative_position(p_lst):
     '''
-    std_line을 기준으로 했을 때, cmp_line과의 교점이 "존재할 수 있는" 조건인지 여부를 return
+    선분을 연장한 직선 위에 점이 있는 경우: 점이 선분 위에 있다면 2, 아니면 0
+    그렇지 않은 경우: 외적이 -이면 1, +이면 0
     '''
-    result = 1
-    for x, y in cmp_line:
-        result *= cross(std_line[1][0] - std_line[0][0], x - std_line[0][0], std_line[1][1] - std_line[0][1], y - std_line[0][1])
-        if result == 0:
-            if include_point(std_line[0], std_line[1], [x, y]):
-                return 2
-            else:
-                break
+    result1 = cross(p_lst[1][0] - p_lst[0][0], p_lst[2][0] - p_lst[0][0], p_lst[1][1] - p_lst[0][1], p_lst[2][1] - p_lst[0][1])
+    result2 = cross(p_lst[1][0] - p_lst[0][0], p_lst[3][0] - p_lst[0][0], p_lst[1][1] - p_lst[0][1], p_lst[3][1] - p_lst[0][1])
+    if (result1 == 0 and include_point(p_lst[0], p_lst[1], p_lst[2])) or (result2 == 0 and include_point(p_lst[0], p_lst[1], p_lst[3])):
+        return 2
 
-    return 1 if result < 0 else 0
-
-
-def is_crossed(line, shape):
-    '''
-    직선 line과 도형 shape 간 교점 존재 여부 return
-    '''
-    answer = 0
-    n = len(shape)
-    for i in range(n):
-        result = 0
-        result += check_relative_position(line, [shape[i], shape[(i+1)%n]])
-        result += check_relative_position([shape[i], shape[(i+1)%n]], line)
-
-        answer += int(result >= 2)
-        
-    return answer > 0
+    return 1 if (result1 * result2 < 0) else 0
     
+p = []
+for i in range(2):
+    a, b, c, d = map(int, input().split())
+    p.append([a, b])
+    p.append([c, d])
 
-t = int(input())
+result = 0
+result += check_relative_position(p)
+result += check_relative_position(p[2:] + p[:2])
 
-for _ in range(t):
-    temp = list(map(int, input().split()))
-    line = [temp[0:2], temp[2:4]]
-    square = [temp[4:6], temp[6:8]]
-
-    answer = 'F'
-    # 선분의 두 점이 직사각형 내에 있는 경우 -> True
-    if include_point(square[0], square[1], line[0]) and include_point(square[0], square[1], line[1]):
-        answer = 'T'
-    # 아닌 경우 -> 검토
-    else:
-        if is_crossed(line, [square[0], [square[1][0], square[0][1]], square[1], [square[0][0], square[1][1]]]):
-            answer = 'T'
-    
-    print(answer)
-
+print(int(result >= 2))
