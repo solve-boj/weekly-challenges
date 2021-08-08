676009dad4c94b25115d0c281e04687c105bdd38
fienestar <fienestar@gmail.com>
2021-08-09 04:56:21 +0900

    Solve: 잘못된 주석 수정

--- 2021W31/fienestar/pgr_82612.cpp
+++ 2021W31/fienestar/pgr_82612.cpp
@@ -1,6 +1,6 @@
 /*
-    약수의 개수와 덧셈
-    https://programmers.co.kr/learn/courses/30/lessons/77884
+    부족한 금액 계산하기
+    https://programmers.co.kr/learn/courses/30/lessons/82612
 */
 #include <algorithm>
 
