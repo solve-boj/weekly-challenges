6475f37d9029d5c075317b01a66284a883091304
B0neh3ad <js1044k@gmail.com>
2021-08-13 17:10:54 +0900

    +1

---
+++ 2021W32/B0neh3ad/pgr_42885.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    sort(people.begin(), people.end());
+
+    vector<bool> visited(people.size(), false);
+    int answer = 0;
+
+    int s = 0, e = people.size() - 1;
+    while(s < e){
+        if(people[s] + people[e] <= limit){
+            visited[s] = true;
+            ++s;
+        }
+        visited[e] = true;
+        --e;
+        ++answer;
+    }
+
+    while(!visited[s]){ // 여기 !people[s]로 했었음 ㅋㅋ
+        ++answer;
+        ++s;
+    }
+
+    return answer;
+}
+
+int main(){
+    cout << solution(vector<int>({10, 10, 10, 10, 10}), 50);
+    return 0;
+}
+/*
+    1트: 답은 다 맞았으나 효율성에서 실패. O(n^2)은 너무 큰 듯.
+    음... 이거 걍 two pointer 아니냐? 그러면 (정렬 포함) O(nlogn) 가능할 듯
+    2트: 분명 맞는 거 같은데 사이트 자체 에러 때문에 패스가 안 됨.
+*/
