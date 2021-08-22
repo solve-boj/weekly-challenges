9ef8cb4d0623848689428ce753d0eddb550ee0a9
HunRotation <48827296+HunRotation@users.noreply.github.com>
2021-08-23 01:51:09 +0900

    +1

--- 2021W33/HunRotation/pgr_70130.cpp
+++ 2021W33/HunRotation/pgr_70130.cpp
@@ -5,17 +5,47 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int last[500000];
+int maxlen[500000];
+
 int solution(vector<int> a) {
+    fill(last, last+a.size(), -1);
     int answer = -1;
+    if (a.size() == 1) return 0;
+
+    for(int i=0;i<a.size();i++) {
+        if(i-1 == last[a[i]]) {
+            if(i<a.size()-1 && a[i] != a[i+1]) {
+                last[a[i]] = i+1;
+                maxlen[a[i]] += 2;
+            }
+        }
+        else {
+            if(a[i] != a[i-1]) {
+                last[a[i]] = i;
+                maxlen[a[i]] += 2;
+            }
+            else if(i<a.size()-1 && a[i] != a[i+1]) {
+                last[a[i]] = i+1;
+                maxlen[a[i]] += 2;
+            }
+        }
+    }
+    
+    for(int i=0;i<a.size();i++) {
+        answer = max(answer, maxlen[i]);
+    }
+
     return answer;
 }
 
 
-// keyword: BFS
-// 시간복잡도: O(N^4)
+// keyword: DP
+// 시간복잡도: O(N)
 /*
 idea: 
 */
