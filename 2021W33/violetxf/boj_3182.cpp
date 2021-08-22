4a94582a5de745830c991d059ef170ecf4133c49
VioletXF <zudevxf@gmail.com>
2021-08-23 04:58:40 +0900

    +1

---
+++ 2021W33/violetxf/boj_3182.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int next[1001];
+    bool visited[1001];
+    for(int i=0;i<n;++i){
+        int recommend;
+        cin>>recommend;
+        recommend--;
+        next[i] = recommend;
+        
+    }
+    int max = 0;
+    int idx = 0;
+    for(int i=0;i<n;++i){
+        for(int k=0;k<n;++k){
+            visited[k] = false;
+        }
+        int current = i;
+        int count = 0;
+        while(!visited[current]){
+            visited[current] = true;
+            current= next[current];
+            count++;
+        }
+        if(count>max){
+            max = count;
+            idx = i;
+        }
+    }
+    cout<<idx+1;
+}
